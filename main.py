4fddade2147dbd96abcd84e0d465810a45372b12
Yusuf Cemal Çelebi <yusufcemalcelebi@gmail.com>
2019-06-11 17:36:44 +0300

    Only Background labeled images removed, files renamed

--- main.py
+++ main.py
@@ -6,17 +6,22 @@ label_path = './labels'
 
 def main():
     tools = utilities(image_path, label_path)
-    """
+
     image_iterator = tools.read_dicom_files()
     label_iterator = tools.read_nifti_files()
-    
+
     dataset_iterator = zip(image_iterator, label_iterator)
 
-    for image, label in dataset_iterator:
+    count = 0
+    for image in image_iterator:
         print(image.shape)
-    """
+        count += 1
 
-    tools.analyze_label_distribution()
+    print(count)
+
+    # tools.rename_files()
+    # tools.analyze_label_distribution()
+    # tools.delete_images()
 
 
 main()
--- utilities.py
+++ utilities.py
@@ -16,8 +16,8 @@ class utilities:
     def read_dicom_files(self):
         patient_name_list = os.listdir(self.image_path)
 
-        for file_name in patient_name_list:
-            patient_folder = os.path.join(self.image_path, file_name)
+        for patient_name in patient_name_list:
+            patient_folder = os.path.join(self.image_path, patient_name)
 
             for dicom_file in os.listdir(patient_folder):
                 dicom_file_path = os.path.join(patient_folder, dicom_file)
@@ -32,11 +32,11 @@ class utilities:
 
                 yield pixel_array
 
-    def read_nifti_files(self):
-        file_name_list = os.listdir(self.label_path)
+    def read_nifti_files(self, label_path='./labels'):
+        file_name_list = os.listdir(label_path)
 
         for file_name in file_name_list:
-            file_path = os.path.join(self.label_path, file_name)
+            file_path = os.path.join(label_path, file_name)
 
             nifti_file = nib.load(file_path)
             slices = nifti_file.get_fdata()
@@ -67,3 +67,53 @@ class utilities:
             rate = count/total_pixel
 
             print("For label " + str(index) + " : %.5f" % rate)
+
+    def rename_files(self):
+        patient_folder_list = os.listdir(self.image_path)
+
+        for patient_folder in patient_folder_list:
+            patient_folder_path = os.path.join(self.image_path, patient_folder)
+
+            for index, dicom_file_name in enumerate(os.listdir(patient_folder_path)):
+                source_path = os.path.join(
+                    patient_folder_path, dicom_file_name)
+                destination_path = os.path.join(
+                    patient_folder_path, str(index) + '-slice')
+
+                os.rename(source_path, destination_path)
+
+    # Delete images only contains background label
+    def delete_images(self):
+        file_name_list = os.listdir(self.label_path)
+        slice_counts = {'1a.nii.gz': 42, '1c.nii.gz': 19, '2a.nii.gz': 30, '2c.nii.gz': 35,
+                        '3a.nii.gz': 188, '4a.nii.gz': 42, '4c.nii.gz': 28, '5c.nii.gz': 46, '7b.nii.gz': 172}
+
+        for file_name in file_name_list:
+            file_path = os.path.join(self.label_path, file_name)
+            patient_id = file_name.split('.')[0]
+
+            nifti_file = nib.load(file_path)
+            slices = nifti_file.get_fdata()
+            slice_count = slices.shape[2]
+
+            new_slices = np.ones((self.img_rows, self.img_columns,
+                                  slice_counts[file_name]), dtype=np.int16)
+
+            new_nifti_file_slice_index = 0
+            for slice_index in range(slice_count):
+                slice = slices[:, :, slice_index]
+                uniqueValues, occurCount = np.unique(slice, return_counts=True)
+
+                if (len(uniqueValues) > 1):
+                    new_slices[:, :, new_nifti_file_slice_index] = slice
+                    new_nifti_file_slice_index += 1
+                else:
+                    patient_folder = os.path.join(self.image_path, patient_id)
+                    dicom_path = os.path.join(patient_folder, str(slice_index))
+
+                    os.remove(dicom_path)
+
+            new_nifti_file = nib.Nifti1Image(new_slices, np.eye(4))
+
+            nib.save(new_nifti_file, os.path.join(
+                './generated', file_name))
